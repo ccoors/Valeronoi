28dfbddc73a95db6ef1c71c431c9a973df42c709
Christian Friedrich Coors <me@ccoors.de>
2021-08-30 21:08:08 +0200

    chore: update build tools

--- tools/appimage/current/02-build_appimage.sh
+++ tools/appimage/current/02-build_appimage.sh
@@ -3,4 +3,4 @@
 set -euo pipefail
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
-docker run --device /dev/fuse --cap-add SYS_ADMIN --env VARIANT=current --rm -it --init -v "${PWD}/../../../":/source valeronoi-appimage-current /source/tools/appimage/.docker/in_docker.sh
+docker run --device /dev/fuse --cap-add SYS_ADMIN --env VARIANT=current --env QMAKE=/usr/lib/qt6/bin/qmake --rm -it --init -v "${PWD}/../../../":/source valeronoi-appimage-current /source/tools/appimage/.docker/in_docker.sh
--- tools/windows/01-build_docker_image.sh
+++ tools/windows/01-build_docker_image.sh
@@ -3,4 +3,4 @@
 set -euo pipefail
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
-docker build -t valeronoi-windows .
+docker build --pull -t valeronoi-windows .
