712dd8a0d143d5075d5a01abba527fe4a54b3c4e
Christian Friedrich Coors <me@ccoors.de>
2022-03-15 15:21:44 +0100

    chore: use GitHub Actions to build Valeronoi

--- tools/appimage/current/01-build_docker_image.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-cd "$(dirname "${BASH_SOURCE[0]}")"
-docker build --pull -t valeronoi-appimage-current .
--- tools/appimage/current/02-build_appimage.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-cd "$(dirname "${BASH_SOURCE[0]}")"
-docker run --device /dev/fuse --cap-add SYS_ADMIN --env VARIANT=current --env QMAKE=/usr/lib/qt6/bin/qmake --rm -it --init -v "${PWD}/../../../":/source valeronoi-appimage-current /source/tools/appimage/.docker/in_docker.sh
